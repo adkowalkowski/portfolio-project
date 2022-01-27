283cd1db4454319ec4ae8e8a3fb4a55cfd29f121
anthony <adkowalkowski@gmail.com>
2022-01-27 17:59:38 -0600

    testing file paths for github pages

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ $(() => {
 
         // Could make img an a tag, but caused complications for me. found on mdn the location.assign method
         $(".anthony-headshot").on("click", function() {
-            window.location.assign("/index.html");
+            window.location.assign("index.html");
         });
 
      
