21aaa70ecfca36d66b103562570ce6ee3d99d42c
anthony <adkowalkowski@gmail.com>
2022-01-26 19:11:11 -0600

    Added hover effects on nav bar, fadeIn animations with jquery, and updated fonts

--- main.js
+++ main.js
@@ -3,4 +3,13 @@ $(() => {
         $(this).toggleClass("project-img-hover");
         }
       );
+      $(document).ready(function(){
+
+        /*! Fades in page on load */
+        $('.name').css('display', 'none');
+        $('.name').slideDown(1250);
+        $('.page').css('display', 'none');
+        $('.page').slideDown(1250);
+        
+        });
 });
