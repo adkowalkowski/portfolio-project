6f9058fcc308b59149f7c087f799e0556327b9d7
anthony <adkowalkowski@gmail.com>
2022-01-26 19:56:32 -0600

    Added jquery so user is redirected to about page when the logo is clicked

--- main.js
+++ main.js
@@ -12,4 +12,12 @@ $(() => {
         $('.page').slideDown(1250);
         
         });
+
+        // Could make img an a tag, but caused complications for me. found on mdn the location.assign method
+        $(".anthony-headshot").on("click", function() {
+            window.location.assign("/index.html");
+        });
+
+       
+        
 });
